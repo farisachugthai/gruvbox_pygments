49c376b64ee32ec74f76d53c2230cd619399774f
farisachugthai <farischugthai@gmail.com>
2019-07-24 18:59:10 -0400

    Add a few IPython specific token additions. Version bump

--- gruvbox/style.py
+++ gruvbox/style.py
@@ -11,10 +11,12 @@ This pygments colorscheme can be used with any API that allows for Pygments
 syntax highlighting. It has; however, been primarily tested with IPython.
 
 """
+from IPython import get_ipython
 from pygments.style import Style
-from pygments.token import (Comment, Name, Keyword, Generic, Number,
-                            Error, Punctuation, Operator, String,
-                            Literal, Escape, Text)
+from pygments.token import (
+    Comment, Name, Keyword, Generic, Number, Error, Punctuation, Operator,
+    String, Literal, Escape, Text, Token
+)
 
 # We're still exporting ALL these globals. Let's not.
 __all__ = ['GruvboxStyle']
@@ -25,7 +27,6 @@ BG0_HARD = "#1d2021"
 dark1 = '#3c3836'  # 60-56-54
 DARK2 = '#504945'  # 80-73-69
 DARK3 = '#665c54'  # 102-92-84
-dark4 = '#7c6f64'  # 124-111-100
 
 # Comments
 GRAY_245 = '#928374'  # 146-131-116
@@ -46,21 +47,7 @@ BRIGHT_PURPLE = '#d3869b'  # 211-134-155
 BRIGHT_AQUA = '#8ec07c'  # 142-192-124
 BRIGHT_ORANGE = '#fe8019'  # 254-128-25
 
-NEUTRAL_RED = '#cc241d'  # 204-36-29
-neutral_green = '#98971a'  # 152-151-26
-NEUTRAL_YELLOW = '#d79921'  # 215-153-33
-neutral_blue = '#458588'  # 69-133-136
-NEUTRAL_PURPLE = '#b16286'  # 177-98-134
-NEUTRAL_AQUA = '#689d6a'  # 104-157-106
 NEUTRAL_ORANGE = '#d65d0e'  # 214-93-14
-
-FADED_RED = '#9d0006'  # 157-0-6
-FADED_GREEN = '#79740e'  # 121-116-14
-FADED_YELLOW = '#b57614'  # 181-118-20
-FADED_BLUE = '#076678'  # 7-102-120
-FADED_PURPLE = '#8f3f71'  # 143-63-113
-faded_aqua = '#427b58'  # 66-123-88
-FADED_ORANGE = '#af3a03'  # 175-58-3
 
 # }}}
 
@@ -79,99 +66,166 @@ class GruvboxStyle(Style):
     # highlight_color = SELECTION
 
     styles = {
-        Comment: GRAY_245,  # class:'c'
-        Comment.Hashbang: GRAY_245,  # class: 'ch'
-        Comment.Multiline: NEUTRAL_YELLOW + ' italic',  # class: 'cm'
+        Comment:
+            GRAY_245,  # class:'c'
+        Comment.Hashbang:
+            GRAY_245,  # class: 'ch'
+        Comment.Multiline:
+            BRIGHT_YELLOW + ' italic',  # class: 'cm'
         # Comment.Preproc: BRIGHT_AQUA,  # class: 'cp'
         # Comment.PreprocFile: 'cpf',
-        Comment.Singleline: GRAY_245 + ' italic',
-        Escape: DARK3,  # class: 'Esc'
-        Error: BRIGHT_RED + ' bold',  # class: 'Err'
-        Generic: FG1,
-        Generic.Deleted: LIGHT0_HARD,
-        Generic.Emph: 'underline ' + BRIGHT_BLUE,
-        Generic.Heading: BRIGHT_GREEN + ' bold',
-        Generic.Inserted: LIGHT0_HARD,
-        Generic.Output: FG2,
-        Generic.Prompt: BRIGHT_BLUE,
-        Generic.Strong: FG1 + ' bold',
-        Generic.Subheading: BRIGHT_GREEN + ' bold',
-        Keyword: BRIGHT_ORANGE,  # class: 'k'
-        Keyword.Constant: FADED_ORANGE,  # class: 'kc'
-        Keyword.Declaration: BRIGHT_ORANGE,  # class: 'kd'
-        Keyword.Type: BRIGHT_YELLOW,  # class: 'kt'
+        Comment.Singleline:
+            GRAY_245 + ' italic',
+        Escape:
+            DARK3,  # class: 'Esc'
+        Error:
+            BRIGHT_RED + ' bold',  # class: 'Err'
+        Generic:
+            FG1,
+        Generic.Deleted:
+            LIGHT0_HARD,
+        Generic.Emph:
+            'underline ' + BRIGHT_BLUE,
+        Generic.Heading:
+            BRIGHT_GREEN + ' bold',
+        Generic.Inserted:
+            LIGHT0_HARD,
+        Generic.Output:
+            FG2,
+        Generic.Prompt:
+            BRIGHT_BLUE,
+        Generic.Strong:
+            FG1 + ' bold',
+        Generic.Subheading:
+            BRIGHT_GREEN + ' bold',
+        Keyword:
+            BRIGHT_ORANGE,  # class: 'k'
+        Keyword.Constant:
+            BRIGHT_ORANGE,  # class: 'kc'
+        Keyword.Declaration:
+            BRIGHT_ORANGE,  # class: 'kd'
+        Keyword.Type:
+            BRIGHT_YELLOW,  # class: 'kt'
         # Keyword.Namespace: 'kn',
 
         # class: 'kp', the NumPy Lexer registers tokens as this class
-        Keyword.Pseudo: NEUTRAL_PURPLE,
+        Keyword.Pseudo:
+            BRIGHT_PURPLE,
         # Keyword.Reserved: 'kr',
         # Literal: BRIGHT_GREEN,  # class: 'l'
         # Literal.Date: 'ld',
-        Literal.Number: BRIGHT_PURPLE,  # class: 'm',
+        Literal.Number:
+            BRIGHT_PURPLE,  # class: 'm',
         # Literal.Number.Bin: 'mb',
         # Literal.Number.Float: 'mf',
         # Literal.Number.Hex: 'mh',
         # Literal.Number.Integer: 'mi',
         # Literal.Number.Integer.Long: 'il',
         # Literal.Number.Oct: 'mo',
-        Literal.String: BRIGHT_GREEN,  # class: 's'
-        Literal.String.Other: BRIGHT_GREEN,  # class: 'sx'
-        Literal.String.Affix: BRIGHT_ORANGE,  # class: 'sa',
+        Literal.String:
+            BRIGHT_GREEN,  # class: 's'
+        Literal.String.Other:
+            BRIGHT_GREEN,  # class: 'sx'
+        Literal.String.Affix:
+            BRIGHT_ORANGE,  # class: 'sa',
 
         # Literally matches `.* lol
-        Literal.String.Backtick: LIGHT4,  # class: 'sb',
+        Literal.String.Backtick:
+            LIGHT4,  # class: 'sb',
         # Literal.String.Char: 'sc',
         # Literal.String.Delimiter: 'dl',
-        Literal.String.Doc: FG1,  # class: 'sd',
+        Literal.String.Doc:
+            FG1,  # class: 'sd',
         # Literal.String.Double: 's2',
         # Literal.String.Escape: 'se',
         # Literal.String.Heredoc: 'sh',
         # Interpolated strings!
-        Literal.String.Interpol: FADED_GREEN,  # class 'si',
-        Literal.String.Regex: BRIGHT_YELLOW,  # class: 'sr',
+        Literal.String.Interpol:
+            BRIGHT_GREEN,  # class 'si',
+        Literal.String.Regex:
+            BRIGHT_YELLOW,  # class: 'sr',
         # Literal.String.Single: 's1',
-        Literal.String.Symbol: FG2,  # class: 'ss',
-        Name: FG1,  # class: 'n'
-        Name.Attribute: BRIGHT_GREEN,  # class: 'na'
-        Name.Builtin: BRIGHT_YELLOW,  # class: 'nb'
+        Literal.String.Symbol:
+            FG2,  # class: 'ss',
+        Name:
+            FG1,  # class: 'n'
+        Name.Attribute:
+            BRIGHT_GREEN,  # class: 'na'
+        Name.Builtin:
+            BRIGHT_YELLOW,  # class: 'nb'
         # Name.Builtin.Pseudo: 'bp',
-        Name.Class: NEUTRAL_ORANGE,  # class: 'nc',
-        Name.Constant: BRIGHT_PURPLE,  # class: 'no'
-        Name.Decorator: BRIGHT_YELLOW,  # class: 'nd'
-        Name.Entity: BRIGHT_YELLOW,  # class: 'ni'
-        Name.Exception: 'bold ' + BRIGHT_RED,  # class: 'ne'
-
-        Name.Function: 'noinherit ' + BRIGHT_YELLOW,  # class: 'nf'
+        Name.Class:
+            NEUTRAL_ORANGE,  # class: 'nc',
+        Name.Constant:
+            BRIGHT_PURPLE,  # class: 'no'
+        Name.Decorator:
+            BRIGHT_YELLOW,  # class: 'nd'
+        Name.Entity:
+            BRIGHT_YELLOW,  # class: 'ni'
+        Name.Exception:
+            'bold ' + BRIGHT_RED,  # class: 'ne'
+        Name.Function:
+            'noinherit ' + BRIGHT_YELLOW,  # class: 'nf'
         # dunder methods
-        Name.Function.Magic: 'noinherit ' + NEUTRAL_AQUA,  # class: 'fm'
-        Name.Label: BRIGHT_RED,  # class: 'nl'
+        Name.Function.Magic:
+            'noinherit ' + BRIGHT_AQUA,  # class: 'fm'
+        Name.Label:
+            BRIGHT_RED,  # class: 'nl'
         # import statements
-        Name.Namespace: FADED_BLUE,  # class: 'nn',
+        Name.Namespace:
+            BRIGHT_BLUE,  # class: 'nn',
         # Name.Other: 'nx',
-        Name.Property: BRIGHT_AQUA,  # class: 'py'
-        Name.Tag: BRIGHT_RED,  # class: 'nt'
-        Name.Variable: FG1,  # class: 'nv'
-        Name.Variable.Class: 'noinherit ' + BRIGHT_BLUE,  # class: 'vc'
-        Name.Variable.Global: 'noinherit ' + BRIGHT_BLUE,  # class: 'vg'
-        Name.Variable.Instance: 'noinherit ' + BRIGHT_BLUE,  # class:'vi'
-        Name.Variable.Magic: 'noinherit ' + BRIGHT_BLUE,  # class: 'vm'
-        Number.Float: BRIGHT_PURPLE,
-        Number: BRIGHT_PURPLE,
-        Operator: BRIGHT_RED,  # class: 'o'
+        Name.Property:
+            BRIGHT_AQUA,  # class: 'py'
+        Name.Tag:
+            BRIGHT_RED,  # class: 'nt'
+        Name.Variable:
+            FG1,  # class: 'nv'
+        Name.Variable.Class:
+            'noinherit ' + BRIGHT_BLUE,  # class: 'vc'
+        Name.Variable.Global:
+            'noinherit ' + BRIGHT_BLUE,  # class: 'vg'
+        Name.Variable.Instance:
+            'noinherit ' + BRIGHT_BLUE,  # class:'vi'
+        Name.Variable.Magic:
+            'noinherit ' + BRIGHT_BLUE,  # class: 'vm'
+        Number.Float:
+            BRIGHT_PURPLE,
+        Number:
+            BRIGHT_PURPLE,
+        Operator:
+            BRIGHT_RED,  # class: 'o'
         # Operator.Word: NEUTRAL_RED,  # class: 'ow'
         # really hard to read
-        Operator.Word: 'noinherit ' + FADED_RED,
-        Punctuation: FG1,  # class: 'p'
-        String.Symbol: BRIGHT_BLUE,
-        String: BRIGHT_GREEN,
-        Text: FG1,  # class: 't'
+        Operator.Word:
+            'noinherit ' + BRIGHT_RED,
+        Punctuation:
+            FG1,  # class: 'p'
+        String.Symbol:
+            BRIGHT_BLUE,
+        String:
+            BRIGHT_GREEN,
+        Text:
+            FG1,  # class: 't'
         # treating this key as Vim's Identifier token.
-        Text.Whitespace: 'underline ' + FADED_YELLOW,  # class: 'w'
+        Text.Whitespace:
+            'underline ' + BRIGHT_YELLOW,  # class: 'w'
     }
 
-    def __init__(self, **kwargs):
-        """Create the colorscheme object."""
-        self.__dict__.update(**kwargs)
+    # I think this is a reasonable way of checking if we're in IPython or not
+    if get_ipython() is not None:
+        styles.update(
+            {
+                Token.Prompt: BRIGHT_GREEN,
+                Token.PromptNum: 'bold {}'.format(BRIGHT_GREEN),
+                Token.OutPrompt: BRIGHT_GREEN,
+                Token.OutPromptNum: BRIGHT_GREEN,
+                # TODO:
+                # Token.Menu.Completions.Completion
+                # Token.Menu.Completions.Completion.Current
+                # Token.MatchingBracket.Other
+            }
+        )
 
     def __repr__(self):
         """Return repr representation."""
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-"""Setup IPython initialization files as it's own package.
-
-Install gruvbox as a colorscheme in pygments, and we'll
-also probably need to add the IPython doctest plugin somewhere.
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup Gruvbox colorscheme as a python package."""
 import codecs
 import os
 from setuptools import setup
@@ -10,70 +8,61 @@ from setuptools import setup
 NAME = 'gruvbox_ipython'
 AUTHOR = "Faris Chugthai",
 EMAIL = "farischugthai@gmail.com",
-DESCRIPTION = "Pygments style using the gruvbox color scheme."
+DESCRIPTION = "A Pygments style using the Gruvbox color scheme."
 LICENSE = "MIT",
 KEYWORDS = "pygments gruvbox ipython jupyter colorschemes",
 REQUIRED = ['pygments']
 
 here = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(here, 'README.rst')
 
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    with codecs.open(README, encoding="utf-8") as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
 about = {}
 
 PROJECT_SLUG = NAME.lower().replace("-", "_").replace(" ", "_")
 
-# For those moments when you forget your iskeyword necessities
 try:
     with open(os.path.join(here, PROJECT_SLUG, '__version__.py')) as f:
         exec(f.read(), about)
 except IOError:  # the file doesn't exist so hard code it
-    about['__version__'] = '0.0.1'
+    about['__version__'] = '0.0.2'
 
 setup(
-        name=NAME,
-        version=about['__version__'],
-        description=DESCRIPTION,
-        long_description=long_description,
-        long_description_content_type='text/restructuredtext',
-        author=AUTHOR,
-        author_email=EMAIL,
-        # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-        # If your package is a single module, use this instead of 'packages':
-        # py_modules=['mypackage'],
-
-        entry_points=
-        """
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/restructuredtext',
+    author=AUTHOR,
+    author_email=EMAIL,
+    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    py_modules=['gruvbox.style'],
+    entry_points="""
         [pygments.styles]
         Gruvbox = gruvbox.style:GruvboxStyle
         """,
-        install_requires=REQUIRED,
-        # is there a way i can do if extras_require inside of the function call?
-        # extras_require=EXTRAS,
-        include_package_data=True,
-        package_data={
-            # If any package contains *.txt or *.rst files, include them:
-            '': ['*.txt', '*.rst'],
-        },
-        license=LICENSE,
-
-        # TODO: Need to install on py2 and make sure this works but pygments
-        # should
-        classifiers=[
-            # Trove classifiers
-            # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy'
-        ],
+    install_requires=REQUIRED,
+    # is there a way i can do if extras_require inside of the function call?
+    # extras_require=EXTRAS,
+    include_package_data=True,
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+    license=LICENSE,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
