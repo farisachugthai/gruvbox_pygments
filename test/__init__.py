9ce599e8f217e48744cecdd96416ed5ae60d0af7
farisachugthai <farischugthai@gmail.com>
2020-01-18 23:02:42 -0500

    Add some pytest

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,4 @@
-"""TODO: Add a package_name parameter to the ask_import function.
 
-The only reason I'm putting functionality here is because it'll be needed
-across the package.
-"""
 import unittest
 
 import gruvbox
@@ -10,4 +6,4 @@ import gruvbox
 try:
     import pytest
 except:  # noqa
-    pass
+    pytest = None
